00fec148e4de82e15eb41bfb11c3becca8497d96
Emory Merryman <emory.merryman@gmail.com>
2023-01-06 20:29:03 -0500

    YES

--- tests/positive/flake.nix
+++ tests/positive/flake.nix
@@ -3,9 +3,10 @@
         {
           nixpkgs.url = "github:nixos/nixpkgs?rev=57eac89459226f3ec743ffa6bbbc1042f5836843" ;
           flake-utils.url = "github:numtide/flake-utils" ;
+          strip.url = "/home/runner/work/strip/strip" ;
         } ;
       outputs =
-        { self , nixpkgs , flake-utils } :
+        { self , nixpkgs , flake-utils , strip } :
           flake-utils.lib.eachDefaultSystem
           (
             system :
@@ -13,9 +14,17 @@
                 devShell =
                   let
                     pkgs = builtins.getAttr system nixpkgs.legacyPackages ;
+		    test = builtins.getAttr system strip.lib ;
 		    in
  			pkgs.mkShell { buildInputs = [
-				( pkgs.writeShellScriptBin "hello" "${ pkgs.coreutils }/bin/echo Hello" )
+				( pkgs.writeShellScriptBin "hello"
+				  ''
+				    ${ pkgs.coreutils }/bin/cat <EOF
+				    Hello
+				    ${ if test "A" == "A" then "YES" else "NO" }
+				    EOF
+				  ''
+				)
 			] ; };
               }
       ) ;
