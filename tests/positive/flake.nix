2e9130812c62c8074ec3ef94c953e4a1218de605
Emory Merryman <emory.merryman@gmail.com>
2023-01-07 20:41:20 -0500

    Maybe this works

--- tests/positive/flake.nix
+++ tests/positive/flake.nix
@@ -21,8 +21,7 @@
                                   ''
                                     ${ pkgs.coreutils }/bin/cat <<EOF
                                     Hello
-                                    ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : "${ name }" ) strip.inputs ) ) }
-                                    ${ strip.inputs.flake-utils.url }
+                                    ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : "${ name }" ) strip.inputs.flake-utils ) ) }
                                     ${ if test "A" == "A" then "YES" else "NO" }
                                     EOF
                                   ''
