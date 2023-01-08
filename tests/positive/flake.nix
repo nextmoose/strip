3d052a424737e91777b8b962ec22ffeaac55ddcb
Emory Merryman <emory.merryman@gmail.com>
2023-01-07 21:17:38 -0500

    test this works

--- tests/positive/flake.nix
+++ tests/positive/flake.nix
@@ -19,20 +19,20 @@
                         pkgs.mkShell { buildInputs = [
                                 ( pkgs.writeShellScriptBin "hello"
                                   ''
-                                    if [ "${ strip.inputs.flake-utils.rev }" == "5aed5285a952e0b949eb3ba02c12fa4fcfef535fX" ]
+                                    if [ "${ strip.inputs.flake-utils.rev }" == "5aed5285a952e0b949eb3ba02c12fa4fcfef535f" ]
                                     then
                                       ${ pkgs.coreutils }/bin/echo The flake-utils version is good ${ strip.inputs.flake-utils.rev }.
                                     else
                                       ${ pkgs.coreutils }/bin/echo The flake-utils version is bad.  We observed ${ strip.inputs.flake-utils.rev } but we expect 5aed5285a952e0b949eb3ba02c12fa4fcfef535f.  &&
                                       exit 64
                                     fi &&
-                                    ${ pkgs.coreutils }/bin/cat <<EOF
-                                    Hello
-                                    ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : "${ name }" ) strip.inputs.flake-utils ) ) }
-                                    BYE
-                                    ${ strip.inputs.flake-utils.rev }
-                                    ${ if test "A" == "A" then "YES" else "NO" }
-                                    EOF
+				    if [ "${ builtins.getAttr system strip.lib "    ca6fed9d-abcd-4c90-9a24-a79abb9b212f   " }" == "ca6fed9d-abcd-4c90-9a24-a79abb9b212fX" ]
+				    then
+				      ${ pkgs.coreutils }/bin/echo The test is good ca6fed9d-abcd-4c90-9a24-a79abb9b212f &&
+				    else
+				      ${ pkgs.coreutils }/bin/echo The test is bad.  We observed ${ builtins.getAttr system strip.lib "   ca6fed9d-abcd-4c90-9a24-a79abb9b212f   " } but we expect ca6fed9d-abcd-4c90-9a24-a79abb9b212f. &&
+				      exit 64
+				    fi &&
                                   ''
                                 )
                         ] ; };
