e434ef667d61fe51c35f6571dcdf634ae7104277
Emory Merryman <emory.merryman@gmail.com>
2023-01-08 16:57:03 -0500

    NOW YES

--- tests/base/flake.nix
+++ tests/base/flake.nix
@@ -19,52 +19,7 @@
                           _utils = builtins.getAttr system utils.lib ;
                           pkgs = builtins.getAttr system nixpkgs.legacyPackages ;
                           program =
-			    let
-                              negatives =
-                                let
-                                  mapper =
-                                    name : value :
-                                      pkgs.writeShellScript
-                                        name
-                                        ''
-                                          PROJECT_DIRECTORY=$( ${ pkgs.coreutils }/bin/pwd ) &&
-                                          cd $( ${ pkgs.mktemp }/bin/mktemp --directory ) &&
-                                          ${ pkgs.nix }/bin/nix flake init &&
-                                          ( ${ pkgs.coreutils }/bin/cat > flake.nix <<EOF
-                                            {
-                                              inputs =
-                                                {
-                                                  flake-utils.url = "github:numtide/flake-utils" ;
-                                                  nixpkgs.url = "github:nixos/nixpkgs" ;
-                                                  testee.url = "${ _utils.bash-variable "PROJECT_DIRECTORY" }" ;
-                                                } ;
-                                              outputs =
-                                                { flake-utils,  nixpkgs , self , testee } : { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "negative" value ) ] ; } ; }
-                                            }
-                                          EOF
-                                          ) &&
-                                          OBSERVED="$( ${ pkgs.nix }/bin/nix develop 2> >( ${ pkgs.coreutils }/bin/tee ) ) &&
-                                          EXPECTED="${ _utils.bash-variable "1" }" &&
-                                          if [ "${ _utils.bash-variable "EXPECTED" }" == "${ _utils.bash-variable "OBSERVED" }" ]
-                                          then
-                                            ( ${ pkgs.coreutils }/bin/cat <<EOF
-                                           POSITIVE="GOOD"
-                                           NAME="${ name }"
-                                          HASH="${ _utils.bash-variable "EXPECTED" }"
-                                          EOF
-                                            )
-                                          else
-                                            ( ${ pkgs.coreutils }/bin/cat <<EOF
-                                          POSITIVE="BAD"
-                                          NAME="${ name }"
-                                          OBSERVED="${ _utils.bash-variable "OBSERVED" }"
-                                          EXPECTED="${ _utils.bash-variable "EXPECTED" }"
-                                          EOF
-                                            ) &&
-                                            exit 64
-                                          fi
-                                        '' ;
-                                  in builtins.mapAttrs mapper ( negatives ( builtins.getAttr system test.lib ) ) ;
+			    {
                               positives =
                                 let
                                   mapper =
@@ -93,7 +48,7 @@
                                             exit 64
                                           fi
                                         '' ;
-                                  in builtins.mapAttrs mapper positives ;
+                                  in builtins.attrNames ( builtins.mapAttrs mapper positives ) ;
                               versions =
                                 let
                                   mapper =
@@ -122,10 +77,10 @@
                                             exit 64
                                         '' ;
                                   in builtins.mapAttrs mapper test ;
-                            in pkgs.writeShellScriptBin "hook" ( builtins.concatStringsSep " &&\n" ( builtins.concatLists ( builtins.map builtins.attrValues [ positives ] ) ) ) ;
+		            } ;
                           in pkgs.mkShell
                             {
-                              buildInputs = [ program ] ;
+                              buildInputs = [ ( builtins.writeShellScriptBin "hook" ( builtins.concatStringsSep " &&\n" ( builtins.concatLists [ positives ] ) ) ) ] ;
                             } ;
                     } ;
               }
