e3c56e60496182e46aac8055ddd46ac595b735a0
Emory Merryman <emory.merryman@gmail.com>
2023-01-08 22:45:41 -0500

    RENAME

--- base/test-versions/flake.nix
+++ base/test-versions/flake.nix
@@ -19,49 +19,45 @@
                           _test = builtins.getAttr system test.lib ;
                           _utils = builtins.getAttr system utils.lib ;
                           pkgs = builtins.getAttr system nixpkgs.legacyPackages ;
-                          programs =
-                            {
-                              versions =
-                                let
-                                  mapper =
-                                    name : value :
-                                      let
-                                        input = builtins.getAttr name test.inputs ;
-                                        in
-                                          pkgs.writeShellScript
-                                            name
-                                            ''
-                                              OBSERVED="${ input.rev }" &&
-                                              EXPECTED="${ value }" &&
-                                              if [ "${ _utils.bash-variable "OBSERVED" }" == "${ _utils.bash-variable "EXPECTED" }" ]
-                                              then
-                                                ( ${ pkgs.coreutils }/bin/cat <<EOF
-                                              #
-                                              TEST="GOOD"
-                                              TYPE="VERSION"
-                                              NAME="${ name }"
-                                              HASH="${ _utils.bash-variable "EXPECTED" }"
-                                              EOF
-                                                )
-                                              else
-                                                ( ${ pkgs.coreutils }/bin/cat <<EOF
-                                              #
-                                              TEST="GOOD"
-                                              TYPE="VERSION"
-                                              NAME="${ name }"
-                                              OBSERVED="${ _utils.bash-variable "OBSERVED" }"
-                                              EXPECTED="${ _utils.bash-variable "EXPECTED" }"
-                                              EOF
-                                                ) &&
-                                                exit 64
-                                              fi
-                                            '' ;
-                                  in builtins.attrValues ( builtins.mapAttrs mapper versions ) ;
+                          mapper =
+                            name : value :
+                              let
+                                input = builtins.getAttr name test.inputs ;
+                                in
+                                  pkgs.writeShellScript
+                                    name
+                                    ''
+                                      OBSERVED="${ input.rev }" &&
+                                      EXPECTED="${ value }" &&
+                                      if [ "${ _utils.bash-variable "OBSERVED" }" == "${ _utils.bash-variable "EXPECTED" }" ]
+                                      then
+                                        ( ${ pkgs.coreutils }/bin/cat <<EOF
+                                      #
+                                      TEST="GOOD"
+                                      TYPE="VERSION"
+                                      NAME="${ name }"
+                                      HASH="${ _utils.bash-variable "EXPECTED" }"
+                                      EOF
+                                        )
+                                      else
+                                        ( ${ pkgs.coreutils }/bin/cat <<EOF
+                                      #
+                                      TEST="GOOD"
+                                      TYPE="VERSION"
+                                      NAME="${ name }"
+                                      OBSERVED="${ _utils.bash-variable "OBSERVED" }"
+                                      EXPECTED="${ _utils.bash-variable "EXPECTED" }"
+                                      EOF
+                                        ) &&
+                                        exit 64
+                                      fi
+                                    '' ;
+                                in builtins.attrValues ( builtins.mapAttrs mapper versions ) ;
                             } ;
                           in pkgs.mkShell
                             {
                               buildInputs =
-                                [ ( pkgs.writeShellScriptBin "test" ( builtins.concatStringsSep " &&\n" ( builtins.concatLists [ programs.versions ] ) ) ) ] ;
+                                [ ( pkgs.writeShellScriptBin "test" ( builtins.concatStringsSep " &&\n" programs.versions ] ) ] ;
                             } ;
                     } ;
               }
