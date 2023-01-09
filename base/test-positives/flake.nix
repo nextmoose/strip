666246adc1df046aec3ee710c29d7a7aeb8221d8
Emory Merryman <emory.merryman@gmail.com>
2023-01-08 22:55:04 -0500

    RENAME

--- base/test-positives/flake.nix
+++ base/test-positives/flake.nix
@@ -19,46 +19,41 @@
                           _test = builtins.getAttr system test.lib ;
                           _utils = builtins.getAttr system utils.lib ;
                           pkgs = builtins.getAttr system nixpkgs.legacyPackages ;
-                          programs =
-                            {
-                              positives =
-                                let
-                                  mapper =
-                                    name : value :
-                                      pkgs.writeShellScript
-                                        name
-                                        ''
-                                          OBSERVED="${ value.observed _test }" &&
-                                          EXPECTED="${ value.expected }" &&
-                                          if [ "${ _utils.bash-variable "EXPECTED" }" == "${ _utils.bash-variable "OBSERVED" }" ]
-                                          then
-                                            ( ${ pkgs.coreutils }/bin/cat <<EOF
-                                          #
-                                          TEST="GOOD"
-                                          TYPE="POSITIVE"
-                                          NAME="${ name }"
-                                          HASH="${ _utils.bash-variable "EXPECTED" }"
-                                          EOF
-                                            )
-                                          else
-                                            ( ${ pkgs.coreutils }/bin/cat <<EOF
-                                          #
-                                          TEST="BAD"
-                                          TYPE="POSITIVE"
-                                          NAME="${ name }"
-                                          OBSERVED="${ _utils.bash-variable "OBSERVED" }"
-                                          EXPECTED="${ _utils.bash-variable "EXPECTED" }"
-                                          EOF
-                                            ) &&
-                                            exit 64
-                                          fi
-                                        '' ;
-                                  in builtins.attrValues ( builtins.mapAttrs mapper positives ) ;
-                            } ;
+                          mapper =
+                            name : value :
+                              pkgs.writeShellScript
+                                name
+                                ''
+                                  OBSERVED="${ value.observed _test }" &&
+                                  EXPECTED="${ value.expected }" &&
+                                  if [ "${ _utils.bash-variable "EXPECTED" }" == "${ _utils.bash-variable "OBSERVED" }" ]
+                                  then
+                                    ( ${ pkgs.coreutils }/bin/cat <<EOF
+                                  #
+                                  TEST="GOOD"
+                                  TYPE="POSITIVE"
+                                  NAME="${ name }"
+                                  HASH="${ _utils.bash-variable "EXPECTED" }"
+                                  EOF
+                                    )
+                                  else
+                                    ( ${ pkgs.coreutils }/bin/cat <<EOF
+                                  #
+                                  TEST="BAD"
+                                  TYPE="POSITIVE"
+                                  NAME="${ name }"
+                                  OBSERVED="${ _utils.bash-variable "OBSERVED" }"
+                                  EXPECTED="${ _utils.bash-variable "EXPECTED" }"
+                                  EOF
+                                    ) &&
+                                    exit 64
+                                  fi
+                                '' ;
+                              in builtins.attrValues ( builtins.mapAttrs mapper positives ) ;
                           in pkgs.mkShell
                             {
                               buildInputs =
-                                [ ( pkgs.writeShellScriptBin "hook" ( builtins.concatStringsSep " &&\n" ( builtins.concatLists [ programs.positives ] ) ) ) ] ;
+                                [ ( pkgs.writeShellScriptBin "hook" ( builtins.concatStringsSep " &&\n" ( builtins.attrNames ( builtins.mapAttrs mapper positives ) ) ) ) ] ;
                             } ;
                     } ;
               }
