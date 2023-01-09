1b010b19f244c5a04d377392e9b5fc283fc097e4
Emory Merryman <emory.merryman@gmail.com>
2023-01-08 19:50:43 -0500

    NOW YES

--- tests/base/flake.nix
+++ tests/base/flake.nix
@@ -29,12 +29,6 @@
                                         name
                                         ''
 					  ${ pkgs.coreutils }/bin/echo 4141181e-ff52-443b-921a-3564d212348c &&
-                                          PROJECT_DIRECTORY=$( ${ pkgs.coreutils }/bin/pwd ) &&
-					  ${ pkgs.coreutils }/bin/echo 42e3f28d-328d-4f5e-8919-6ffdbe89aaa8 &&
-                                          cd $( ${ pkgs.mktemp }/bin/mktemp --directory ) &&
-					  ${ pkgs.coreutils }/bin/echo 1ebabaa8-1c7d-4918-9a28-c2e4131f1f5a &&
-                                          ${ pkgs.nix }/bin/nix flake init &&
-					  ${ pkgs.coreutils }/bin/echo b2a86e6d-a4a0-4a69-b4d9-9a3f8b9294ee &&
 					  ${ pkgs.coreutils }/bin/true
                                         '' ;
                                   in builtins.attrValues ( builtins.mapAttrs mapper negatives ) ;
