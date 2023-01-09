8be271f11cd2a6b2cf7bb79786230656ce9c6b23
Emory Merryman <emory.merryman@gmail.com>
2023-01-08 23:25:41 -0500

    RENAME

--- util/test-positives/flake.nix
+++ util/test-positives/flake.nix
@@ -3,7 +3,7 @@
         {
           flake-utils.url = "github:numtide/flake-utils" ;
           strip.url = "/home/runner/work/strip/strip" ;
-          test-utils.url = "/home/runner/work/strip/strip/base/test-versions" ;
+          test-utils.url = "/home/runner/work/strip/strip/base/test-positives" ;
         } ;
       outputs =
         { self , nixpkgs , flake-utils , strip , test-utils } :
