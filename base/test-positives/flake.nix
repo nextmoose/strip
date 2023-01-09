8c48dfe5e837adbe3d1233b92c27e1e2ccc98483
Emory Merryman <emory.merryman@gmail.com>
2023-01-08 22:58:49 -0500

    RENAME

--- base/test-positives/flake.nix
+++ base/test-positives/flake.nix
@@ -53,7 +53,7 @@
                           in pkgs.mkShell
                             {
                               buildInputs =
-                                [ ( pkgs.writeShellScriptBin "test" ( builtins.concatStringsSep " &&\n" ( builtins.attrNames ( builtins.mapAttrs mapper positives ) ) ) ) ] ;
+                                [ ( pkgs.writeShellScriptBin "test1" ( builtins.concatStringsSep " &&\n" ( builtins.attrValues ( builtins.mapAttrs mapper positives ) ) ) ) ] ;
                             } ;
                     } ;
               }
