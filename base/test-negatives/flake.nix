0a729d20005875f3d4bde816a11a64496450f738
Emory Merryman <emory.merryman@gmail.com>
2023-01-14 18:39:17 -0500

    YES NOW

--- base/test-negatives/flake.nix
+++ base/test-negatives/flake.nix
@@ -29,7 +29,8 @@
                                         system :
                                           let
                                             pkgs = builtins.getAttr system nixpkgs.legacyPackages ;
-                                            in pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" ${ value ( builtins.getAttr system test.lib ) } ) ] ; }
+					    testee = builtins.getAttr system test.lib ;
+                                            in pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" ${ value "( testee )" } ) ] ; }
                                       ) ;
                                 }
                               '' ;
