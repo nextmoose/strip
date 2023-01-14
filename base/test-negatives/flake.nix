1933c166b81343adc3c15946a840592c64cd6514
Emory Merryman <emory.merryman@gmail.com>
2023-01-14 09:52:35 -0500

    fixed 2 problems

--- base/test-negatives/flake.nix
+++ base/test-negatives/flake.nix
@@ -27,11 +27,11 @@
                                   ( ${ pkgs.coreutils }/bin/cat > flake.nix <<EOF
                                   {
                                     inputs = { flake-utils.url = "github:numtide/flake-utils" ; nixpkgs.url = "github:nixos/nixpkgs" ; test = "/home/runner/work/strip/strip" ; } ;
-                                    outputs = { self , nixpkgs , test } : flake-utils.lib.eachDefaultSystem ( system : ${ value.observed "test" } )
+                                    outputs = { self , nixpkgs , test } : flake-utils.lib.eachDefaultSystem ( system : ${ value.observed "test" } ) ;
                                   }
                                   EOF
                                   ) &&
-                                  ! OBSERVED="$( ${ pkgs.nix }/bin/nix develop --command check > >( ${ pkgs.coreutils }/bin/tee ) )" &&
+                                  ! OBSERVED="$( ${ pkgs.nix }/bin/nix develop --command check 2> >( ${ pkgs.coreutils }/bin/tee ) )" &&
                                   EXPECTED="${ value.expected }" &&
                                   if [ "${ _utils.bash-variable "EXPECTED" }" == "${ _utils.bash-variable "OBSERVED" }" ]
                                   then
