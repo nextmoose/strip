dd0e1752e0b0ebc3200a9254463a3994d8349815
Emory Merryman <emory.merryman@gmail.com>
2023-01-14 07:18:43 -0500

    simpler

--- util/test-versions/flake.nix
+++ util/test-versions/flake.nix
@@ -2,19 +2,13 @@
       inputs =
         {
           flake-utils.url = "github:numtide/flake-utils" ;
+	  strip.url = "/home/runner/work/strip/strip" ;
 	  test-utils.url = "/home/runner/work/strip/strip/base/test-versions" ;
-          nixpkgs.url = "github:nixos/nixpkgs" ;
         } ;
       outputs =
-        { self , flake-utils , nixpkgs , test-utils } :
+        { self , flake-utils , strip , test-utils } :
           flake-utils.lib.eachDefaultSystem
           (
-            system :
-              let
-                pkgs = builtins.getAttr system nixpkgs.legacyPackages ;
-                in
-		  {
-		    devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "${ pkgs.coreutils }/bin/echo PASSED" ) pkgs.coreutils ] ; } ;
-		  }
+            system : builtins.getAttr system test-utils.lib strip {  flake-utils = "5aed5285a952e0b949eb3ba02c12fa4fcfef535fNOSUCH" ; no-such = "findme" ; }
           ) ;
     }
