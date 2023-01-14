87903f227382ff26382a4cdd635ae91e1c497450
Emory Merryman <emory.merryman@gmail.com>
2023-01-14 07:14:36 -0500

    simpler

--- util/test-versions/flake.nix
+++ util/test-versions/flake.nix
@@ -2,10 +2,11 @@
       inputs =
         {
           flake-utils.url = "github:numtide/flake-utils" ;
+	  test-utils.url = "/home/runner/work/strip/strip/base/test-versions" ;
           nixpkgs.url = "github:nixos/nixpkgs" ;
         } ;
       outputs =
-        { self , flake-utils , nixpkgs } :
+        { self , flake-utils , nixpkgs , test-utils } :
           flake-utils.lib.eachDefaultSystem
           (
             system :
