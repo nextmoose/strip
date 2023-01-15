defffbb09c35eff97c19f1d824b43b07d2cc8b61
Emory Merryman <emory.merryman@gmail.com>
2023-01-15 18:31:49 -0500

    40

--- base/update-versions/flake.nix
+++ base/update-versions/flake.nix
@@ -18,37 +18,22 @@
                         let
                           _test = builtins.getAttr system test.lib ;
                           _utils = builtins.getAttr system utils.lib ;
+			  file = builtins.import ./original.flake.nix ;
                           pkgs = builtins.getAttr system nixpkgs.legacyPackages ;
                           mapper =
                             name : value :
                               let
                                 input = builtins.getAttr name test.inputs ;
+				x = builtins.getAttr "original" ( builtins.getAttr name file ) ;
                                 in
                                   builtins.toString ( pkgs.writeShellScript
                                     name
                                     ''
-                                      ( ${ pkgs.coreutils }/bin/cat <<EOF
-                                      ${ _utils.visit { lambda = track : "<LAMBDA>" ; list = track : builtins.concatStringsSep " ;\n" track.reduced ; set = track : builtins.concatStringsSep " ;\n" ( builtins.attrValues ( builtins.mapAttrs ( n : v : "${ n } = ${ v }" ) track.reduced ) ) ; string = track : track.reduced ; undefined = track : builtins.toString track.reduced ; } value }
-                                      __
-                                      __
-                                      _ 
-                                      NAME=${ name }
-                                      ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( n : v : "${ n } = ${ if builtins.typeOf v == "string" then v else "NOT A STRING" }" ) value ) ) }
-                                      _ inputs                     
-                                      ${ builtins.concatStringsSep "\n" ( builtins.attrNames value.inputs ) }
-                                      _ outputs                     
-                                      ${ builtins.concatStringsSep "\n" ( builtins.attrNames value.outputs ) }
-                                      _ lib
-                                      ${ builtins.concatStringsSep "\n" ( builtins.attrNames value.lib ) }
-                                      _ sourceInfo
-                                      ${ builtins.concatStringsSep "\n" ( builtins.attrNames value.sourceInfo ) }
-                                      _ templates 
-                                      ${ builtins.concatStringsSep "\n" ( builtins.attrNames value.templates ) }
-                                      -- ${ value.sourceInfo.outPath }
-                                      EOF
-                                      ) &&
-                                      ${ pkgs.findutils }/bin/find ${ value.sourceInfo.outPath } -name ".git"
-                                      
+				      ${ pkgs.coreutils }/bin/echo <<EOF
+				      ${ name }
+				      ${ value.rev }
+				      ${ x.owner }
+				      EOF
                                     '' ) ;
                           in pkgs.mkShell
                             {
