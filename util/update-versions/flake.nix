b9c9cfca6527d4a22b82b093940e46f4bd5932c3
Emory Merryman <emory.merryman@gmail.com>
2023-01-15 16:24:37 -0500

    forgot 8

--- util/update-versions/flake.nix
+++ util/update-versions/flake.nix
@@ -6,7 +6,7 @@
 	  update-utils.url = "/home/runner/work/strip/strip/base/update-versions" ;
         } ;
       outputs =
-        { self , flake-utils , strip , test-utils } :
+        { self , flake-utils , strip , update-utils } :
           flake-utils.lib.eachDefaultSystem
           (
             system : builtins.getAttr system update-utils.lib strip {  flake-utils = "5aed5285a952e0b949eb3ba02c12fa4fcfef535f" ; }
