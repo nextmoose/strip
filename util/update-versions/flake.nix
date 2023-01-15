de539c3007614e84543331e4dbf397804f57a7cf
Emory Merryman <emory.merryman@gmail.com>
2023-01-15 15:42:38 -0500

    2nd phase of update versions

--- util/update-versions/flake.nix
+++ util/update-versions/flake.nix
@@ -7,8 +7,5 @@
         } ;
       outputs =
         { self , flake-utils , strip , test-utils } :
-          flake-utils.lib.eachDefaultSystem
-          (
-            system : builtins.getAttr system test-utils.lib strip {  flake-utils = "5aed5285a952e0b949eb3ba02c12fa4fcfef535f" ; }
-          ) ;
+          flake-utils.lib.eachDefaultSystem ;
     }
