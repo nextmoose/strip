3fd05bf90139d2f826d99870a1d1f4e39e36931f
Emory Merryman <emory.merryman@gmail.com>
2023-03-08 19:04:17 -0500

    restart

--- flake.nix
+++ flake.nix
@@ -1,29 +1,28 @@
   {
       inputs =
         {
-          flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f" ;
+          flake-utils.url = "github:numtide/flake-utils" ;
+	  shell.url = "github:nextmoose/shell" ;
         } ;
       outputs =
-        { self , flake-utils } :
+        { self , flake-utils , shell } :
           flake-utils.lib.eachDefaultSystem
           (
             system :
               {
-                lib =
-		  let
-		    strip =
-		      string :
+                devShell =
+		  {
+		    lib =
+		      hook : inputs :
 		        let
-			  append = builtins.substring 1 m string ;
-			  is-appended = is-whitespace ( builtins.substring m 1 string ) ;
-			  is-prepended = is-whitespace ( builtins.substring 0 1 string ) ;
-			  is-whitespace = character : builtins.any ( w : w == character ) whitespace ;
-			  n = builtins.stringLength string ;
-			  m = n - 1 ;
-			  prepend = builtins.substring 0 m string ;
-		          whitespace = [ " " "\t" "\n" "\r" "\v" ] ;
-			  in if builtins.typeOf string == "string" then if is-prepended then strip append else if is-appended then strip prepend else string else builtins.throw "668ec04c-a40c-45bc-b66d-de439acf4384" ;
-		    in string : strip string ;
+			  scripts =
+			    {
+			      boot =
+			        {
+				} ;
+			    } ;
+			  in pkgs.mkShell { buildInputs = inputs ( scripts ) ; inputHook = hook ( scripts ) ; }
+		  } ;
               }
       ) ;
     }
